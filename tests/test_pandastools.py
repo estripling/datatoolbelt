2ad05fae0cf39208c6a5417f0ff292b957006b21
estripling <estripling042@gmail.com>
2022-09-24 15:19:58 +0200

    feat(pandastools.py): add freq_columns

--- tests/test_pandastools.py
+++ tests/test_pandastools.py
@@ -148,6 +148,30 @@ def test_freq(datatype, dropna):
     pd.testing.assert_frame_equal(actual, expected)
 
 
+def test_freq_columns():
+    df = pd.DataFrame(
+        {
+            "x": ["a", "c", "b", "g", "h", "a", "g", "a"],
+            "y": [10, 30, 20, 40, 50, 10, 40, 10],
+        }
+    )
+    actual = pandastools.freq_columns(df)
+    rows = [
+        dict(n=3, N=3, r=0.375, R=0.375),
+        dict(n=2, N=5, r=0.250, R=0.625),
+        dict(n=1, N=6, r=0.125, R=0.750),
+        dict(n=1, N=7, r=0.125, R=0.875),
+        dict(n=1, N=8, r=0.125, R=1.000),
+    ]
+    expected = {
+        "x": pd.DataFrame(rows, index=["a", "g", "c", "b", "h"]),
+        "y": pd.DataFrame(rows, index=[10, 40, 30, 20, 50]),
+    }
+    assert isinstance(actual, dict)
+    for column in actual:
+        pd.testing.assert_frame_equal(actual[column], expected[column])
+
+
 def test_join_dataframes_by_index():
     idx = (0, 1)
 
